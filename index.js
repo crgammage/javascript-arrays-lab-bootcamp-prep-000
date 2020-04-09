59daa66b3290e36cdac5a74876911ac4912d8955
masterful-array-3850 <crgammage@gmail.com>
2020-04-09 23:43:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,6 @@ function destructivelyRemoveLastKitten(name) {
 function destructivelyRemoveFirstKitten(name) {
   kittens.shift();
 }
+function appendKitten(name) {
+  return[...kittens, "Broom"]
+}
