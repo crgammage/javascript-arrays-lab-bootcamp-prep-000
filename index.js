046cc315a202ecc653883c6d22529d8522b81284
masterful-array-3850 <crgammage@gmail.com>
2020-04-09 23:49:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function prependKitten(name) {
 function removeLastKitten(name) {
   return kittens.slice(0, kittens.length - 1);
 }
-function removeFirstKitten() {}
+function removeFirstKitten(name) {}
   kittens.slice(1)
 }
